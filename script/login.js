9f43a18ad7a2487d3df0548c6725f8cca9fc8602
Avijeet <avijeet.cse1.bu@gmail.com>
2026-03-13 20:53:53 +0600

    tailwind, daisyui related issue solved and header navbar section is added successfully

---
+++ script/login.js
@@ -0,0 +1,14 @@
+document.getElementById("signIn-btn").addEventListener('click', function() {
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    const usernameValue = username.value;
+    const pwdValue = password.value;
+
+    if (usernameValue === "admin" && pwdValue === "admin123") {
+        window.location.assign('/home.html');
+    }
+    else {
+        alert('Sign In Failed, Try Again');
+        return;
+    }
+})
