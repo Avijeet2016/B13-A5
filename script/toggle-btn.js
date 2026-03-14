887f2bb60cd93f39e9d547579066ee7a688f514f
Avijeet <avijeet.cse1.bu@gmail.com>
2026-03-14 21:57:48 +0600

    closed btn is working

--- script/toggle-btn.js
+++ script/toggle-btn.js
@@ -1,5 +1,6 @@
 const allBtn = document.querySelectorAll('.btn');
 const cardContainer = document.getElementById("card-container");
+const totalIssues = document.getElementById("total-issue");
 
 
 
@@ -25,9 +26,15 @@ const labelStyle = {
   },
   enhancement: {
     class:
-      "badge badge-outline font-semibold text-xs bg-[#DEFCE8] text-[#00A96E] whitespace-",
+      "badge badge-outline font-semibold text-xs bg-[#DEFCE8] text-[#00A96E]",
     icon: "fa-solid fa-star",
     text: "ENHANCEMENT",
+  },
+  documentation: {
+    class:
+      "badge badge-accent badge-outline font-semibold text-xs bg-[#F1FCF9]",
+    icon: "fa-solid fa-book-medical", 
+    text: "DOCUMENTATION",
   },
 };
 
@@ -41,6 +48,11 @@ const loadIssues = async () => {
 
 const displayAllIssues = (data) => {
     cardContainer.innerHTML = ""; 
+    totalIssues.innerHTML = "";
+    const newSpan = document.createElement('span');
+    newSpan.innerHTML = `${data.length}`;
+    totalIssues.appendChild(newSpan);
+
     data.forEach(item => { 
         const newDiv = document.createElement("div");
 
@@ -82,9 +94,14 @@ const loadOpen = async () => {
 }
 
 const displayOpen = (data) => {
+    const statusOpen = data.filter((item) => item.status === "open");
     cardContainer.innerHTML = "";
-    const statusOpen = data.filter(item => item.status === "open");
+    totalIssues.innerHTML = "";
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = `${statusOpen.length}`;
+    totalIssues.appendChild(newSpan);
 
+    
     statusOpen.forEach((item) => {
         const newDiv = document.createElement("div");
 
@@ -119,9 +136,51 @@ const displayOpen = (data) => {
 
 
 const loadClosed = async () => {
-    console.log("Closed btn worked");
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+    const res = await fetch(url);
+    const data = await res.json();
+    displayClosed(data.data);
 }
 
+const displayClosed = (data) => {
+    const statusClosed = data.filter((item) => item.status === "closed");
+    cardContainer.innerHTML = "";
+    totalIssues.innerHTML = "";
+    const newSpan = document.createElement("span");
+    newSpan.innerHTML = `${statusClosed.length}`;
+    totalIssues.appendChild(newSpan);
+    console.log(statusClosed);
+    statusClosed.forEach((item) => {
+    const newDiv = document.createElement("div");
+
+    let labelsHtml = "";
+    item.labels.forEach((i) => {
+        if (labelStyle[i]) {
+            labelsHtml += `<div class="${labelStyle[i].class}"><i class="${labelStyle[i].icon}"></i> ${labelStyle[i].text}</div>`;
+        }
+    });
+
+    newDiv.innerHTML = `
+                <div class="card  bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
+                        <div class="flex justify-between items-center px-4 pt-4">
+                            <img src="${item.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status .png"}" alt="Open/Closed Image">
+                            <div class="badge ${currentPriority[item.priority]} font-bold px-5">${item.priority.toUpperCase()}</div>
+                            </div>
+                        <h1 class="px-4 text-sm font-bold">${item.title}</h1>
+                        <p class="line-clamp-2 px-4 text-xs text-[#64748B]">${item.description}</p>
+                        <div class="px-4 flex flex-wrap gap-1">
+                            ${labelsHtml}
+                        </div>
+                        <hr class="text-gray-300 w-full">
+                        <div class="px-4 pb-4">
+                            <p class="text-[#64748B]">#1 by ${item.author}</p>
+                            <p class="text-[#64748B]">${new Date(item.createdAt).toLocaleDateString("en-US")}</p>
+                        </div>
+                    </div>
+                `;
+    cardContainer.appendChild(newDiv);
+  });
+};  
 
 document.getElementById("toggle-btn").addEventListener('click', function(e) {
     if (e.target.classList.contains('btn')) {
