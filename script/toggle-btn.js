d597d2b82f31c9d3d9c7de326dbbd102cc432299
Avijeet <avijeet.cse1.bu@gmail.com>
2026-03-15 00:20:45 +0600

    modal is working

--- script/toggle-btn.js
+++ script/toggle-btn.js
@@ -1,7 +1,7 @@
 const allBtn = document.querySelectorAll('.btn');
 const cardContainer = document.getElementById("card-container");
 const totalIssues = document.getElementById("total-issue");
-
+const cardDetails = document.getElementById("card-details");
 
 
 const currentPriority = {
@@ -64,7 +64,7 @@ const displayAllIssues = (data) => {
         });
 
         newDiv.innerHTML = `
-        <div class="card  bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
+        <div onclick="loadCardDetails(${item.id})" class="card  bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
                 <div class="flex justify-between items-center px-4 pt-4">
                     <img src="${item.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status .png"}" alt="Open/Closed Image">
                     <div class="badge ${currentPriority[item.priority]} font-bold px-5">${item.priority.toUpperCase()}</div>
@@ -113,7 +113,7 @@ const displayOpen = (data) => {
         });
 
         newDiv.innerHTML = `
-                <div class="card  bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
+                <div onclick="loadCardDetails(${item.id})" class="card bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
                         <div class="flex justify-between items-center px-4 pt-4">
                             <img src="${item.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status .png"}" alt="Open/Closed Image">
                             <div class="badge ${currentPriority[item.priority]} font-bold px-5">${item.priority.toUpperCase()}</div>
@@ -161,7 +161,7 @@ const displayClosed = (data) => {
     });
 
     newDiv.innerHTML = `
-                <div class="card  bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
+                <div onclick="loadCardDetails(${item.id})" class="card bg-[#FFFFFF] card-md shadow-sm ${item.status === "open" ? "border-t-4 border-[#00A96E]" : "border-t-4 border-[#A855F7]"} space-y-4"> 
                         <div class="flex justify-between items-center px-4 pt-4">
                             <img src="${item.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status .png"}" alt="Open/Closed Image">
                             <div class="badge ${currentPriority[item.priority]} font-bold px-5">${item.priority.toUpperCase()}</div>
@@ -198,3 +198,50 @@ document.getElementById("toggle-btn").addEventListener('click', function(e) {
     }
 })
 
+const loadCardDetails = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCardDetails(data.data);
+};
+
+const displayCardDetails = (data) => {
+    cardDetails.innerHTML = "";
+    console.log(data);
+    let labelsHtml = "";
+    data.labels.forEach((i) => {
+      if (labelStyle[i]) {
+        labelsHtml += `<div class="${labelStyle[i].class}"><i class="${labelStyle[i].icon}"></i> ${labelStyle[i].text}</div>`;
+      }
+    });
+    cardDetails.innerHTML = `
+        <h1 class="text-2xl font-bold">${data.title}</h1>
+                    <div class="flex items-center gap-5">
+                        <div class="badge bg-[#00A96E] text-xs font-bold text-white px-5">${data.status.charAt(0).toUpperCase()+data.status.slice(1)}</div>
+                        <div>
+                            <ul class="list-disc text-gray-500 flex gap-5 text-xs">
+                                <li>Opened by ${data.author}</li>
+                                <li>${new Date(data.createdAt).toLocaleDateString("en-US")}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="px-4 flex flex-wrap gap-1">
+                            ${labelsHtml}
+                    </div>
+                    <p class="line-clamp-2 text-md text-[#64748B]">${data.description}</p>
+                    <div class="grid grid-cols-2 items-start bg-[#F8FAFC] p-4">
+                        <div>
+                            <p class="text-[#64748B]">Assignee:</p>
+                            <p class="font-bold">${data.assignee? data.assignee : "N/A"}</p>
+                        </div>
+                        <div>
+                            <p class="text-[#64748B]">Priority:</p>
+                            <div class="badge bg-[#EF4444] font-semibold text-xs text-white">${data.priority.toUpperCase()}</div>
+                        </div>
+                    </div>
+    `;
+    document.getElementById("my_modal_5").showModal();
+}
+
+
+loadIssues();
